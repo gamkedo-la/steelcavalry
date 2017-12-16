3527ad9c57c5abfcd070ac58417f6cfa249dfde4
Chris DeLeon <gamkedo@gmail.com>
2017-12-15 21:13:30 -0800

    Very basic start. WASD/arrows move, spacebar enters/leaves mechs

---
+++ Assets/Mech.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech : MonoBehaviour {
+	public float mechSpeed = 2.0f;
+ 	public float jumpPower = 10.0f;
+	public bool isOnGround;
+	private Rigidbody2D mechRB;
+
+	// Use this for initialization
+	void Start () {
+		mechRB = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	public void MechUpdate () {
+		transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") * Time.deltaTime * mechSpeed;
+
+		if(Input.GetAxisRaw("Vertical") > 0.0f && isOnGround) {
+			mechRB.AddForce(Vector2.up * Input.GetAxisRaw("Vertical") * jumpPower);
+			isOnGround = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D bumpFacts) {
+		for(int i = 0; i < bumpFacts.contacts.Length; i++) {
+			if(bumpFacts.contacts[i].normal.y >= 0.9f) {
+				isOnGround = true;
+				return;
+			}
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float humanSpeed = 0.8f;
+	public float mechNearEnoughToUseDistance = 1.0f;
+	public float exitMechDistancePopUp = 1.1f;
+	public float jetPackPower = 1.0f;
+
+	private Mech mechImIn = null;
+	private Rigidbody2D rb;
+
+	private int mechOnlyMask;
+	private int groundOnlyMask;
+
+	// Use this for initialization
+	void Start () {
+		mechOnlyMask = LayerMask.GetMask("Mech");
+		groundOnlyMask = LayerMask.GetMask("Ground");
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void EnteringOrLeavingMech() {
+		GetComponent<BoxCollider2D>().enabled = (mechImIn == null);
+		rb.velocity = Vector2.zero;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(mechImIn) {
+			mechImIn.MechUpdate();
+			transform.position = mechImIn.transform.position;
+		} else {
+			transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") * Time.deltaTime * humanSpeed;
+			if(Input.GetAxisRaw("Vertical") > 0.0f) {
+				transform.position += Vector3.up * Time.deltaTime * jetPackPower;
+				rb.gravityScale = 0.0f;
+				rb.velocity = Vector2.zero;
+			} else {
+				rb.gravityScale = 1.0f;
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+
+			if(mechImIn) {
+				mechImIn = null;
+				transform.position += Vector3.up * exitMechDistancePopUp;
+				EnteringOrLeavingMech();
+			} else { // entering mech
+				Collider2D[] nearbyMechs = Physics2D.OverlapCircleAll(transform.position,
+													mechNearEnoughToUseDistance,
+													mechOnlyMask);
+				float nearestMechDist = 9000.0f;
+				Collider2D nearestMechCollider = null;
+				for(int i = 0; i < nearbyMechs.Length; i++) {
+					float distToMech = Vector2.Distance(transform.position,
+						                   nearbyMechs[i].transform.position);
+					if(distToMech < nearestMechDist) {
+						nearestMechDist = distToMech;
+						nearestMechCollider = nearbyMechs[i];
+					}
+				}
+				if(nearestMechCollider) {
+					Mech mScript = nearestMechCollider.GetComponent<Mech>();
+					if(mScript) {
+						mechImIn = mScript;
+					}
+					EnteringOrLeavingMech();
+				} // if nearestMechCollider
+			} // else (entering mech)
+		} // input to get in mech
+	} // playerMovement function
+
+
+} // class
