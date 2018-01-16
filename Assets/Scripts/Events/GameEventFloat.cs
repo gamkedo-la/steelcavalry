f76d2e706e9aa2c90432aa413b5f7b5c6ae6b48c
Kornel <Kornel@Kornel.com>
2018-01-16 16:17:11 +0100

    Added comments

--- Assets/Scripts/Events/GameEventFloat.cs
+++ Assets/Scripts/Events/GameEventFloat.cs
@@ -1,26 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu] // So this script appears in Unity's context menu
 public class GameEventFloat : ScriptableObject
 {
+	// List of all GameEventListenerFloat that want to be notified when this event is fired
 	private List<GameEventListenerFloat> listeners = new List<GameEventListenerFloat>();
 
+	// Called when we, from our code, send an event
 	public void Raise( float parameter )
 	{
+		// Back to front so we can unsubscribe from within the loop and still be ok
 		for ( int i = listeners.Count - 1; i >= 0; i-- )
 		{
-			listeners[i].OnEventRaised( parameter );
+			listeners[i].OnEventRaised( parameter ); // Notifies GameEventListenerFloat that this event has been fired
 		}
 	}
 
 	public void Subscribe ( GameEventListenerFloat listener )
 	{
-		listeners.Add( listener );
+		listeners.Add( listener ); // This is the place GameEventListenerFloat adds itself
 	}
 
 	public void UnSubscribe( GameEventListenerFloat listener )
 	{
-		listeners.Remove( listener );
+		listeners.Remove( listener ); // And removes itself
 	}
 }
--- Assets/Scripts/Events/GameEventListenerFloat.cs
+++ Assets/Scripts/Events/GameEventListenerFloat.cs
@@ -2,29 +2,31 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Events;
 
-[System.Serializable]
-public class UnityEventFloat : UnityEvent<float> {}
+[System.Serializable] // So it shows in the inspector
+public class UnityEventFloat : UnityEvent<float> { } // We inherit from UnityEvent so we can pass a float
 
 public class GameEventListenerFloat : MonoBehaviour
 {
-	[SerializeField] private GameEventFloat gameEvent;
-	[SerializeField] private UnityEventFloat response;
+	[SerializeField] private GameEventFloat gameEvent; // Game event we will listen to
+	[SerializeField] private UnityEventFloat response;	// Normal Unity event (the one we created above) to call our own code
 
 	void Start ()
 	{
-		Assert.IsNotNull( gameEvent );
+		Assert.IsNotNull( gameEvent ); // Just making sure the event is set up in the inspector
 	}
 
 	private void OnEnable( )
 	{
+		// We register ourselves with UnityEventFloat - we want to be notified when the event is called
 		gameEvent.Subscribe( this );
 	}
 
 	private void OnDisable( )
 	{
-		gameEvent.UnSubscribe( this );
+		gameEvent.UnSubscribe( this );  // And now we unregister ourselves
 	}
 
+	// This is called by UnityEventFloat when the event is fired
 	public void OnEventRaised ( float parameter )
 	{
 		response.Invoke( parameter );
