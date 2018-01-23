b1c9da6fd0fdacaf3c229f40e327998c0d1b660f
Miles Robson <miles_robson@yahoo.com>
2018-01-22 20:55:45 -0500

    Removed RigidBody from Mine, added basic on collide/damage system

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mine : MonoBehaviour {
+public class Mine : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public float damagePerMine = 10f;
+
+    // Use this for initialization
+    public void Start()
+    {
+
+        //TODO: Random rotation for mixing it up
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+		// TODO: Are Mines static? Maybe they float around!
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Try to find a Mech script on the hit object
+        Mech mechInstance = collision.collider.GetComponent<Mech>();
+        if (mechInstance)
+        {
+			//TODO: Add DoDestruction implementation 
+			Debug.Log("hit a mech");
+			mechInstance.TakeDamage(damagePerMine);
+			//TODO: Add explosion effect
+			Destroy(gameObject);
+        }
+
+
+    }
 }
