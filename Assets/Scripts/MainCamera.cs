ec92c18709702049ebbe4109859b3b60cd10f383
Randy Tan Shaoxian <ranmaru90@gmail.com>
2018-02-04 04:02:04 +0800

    Camera zooms in when player is in a mech, zooms out when player is not in a mech.

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -3,34 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainCamera : MonoBehaviour {
-
-	private float targetCamZoomSize, mechCamZoomSize;
-	private Camera mainCam;
-	private float camZoomK = 0.95f;
-	//
-	public GameObject myPlayer;
-
+    public float outMechCamZoomSize = 20;
+    public float inMechCamZoomSize = 60;
+    public float zoomSpeed = 3;
+    public float followSpeed = 3;
+  
+    private float targetCamZoomSize;
+	private Camera mainCam;	
+    
+	public GameObject myPlayer;    
 
 	// Use this for initialization
 	void Start () {
 		mainCam = Camera.main;
-		targetCamZoomSize = 4;
-		mechCamZoomSize = 6;
+		targetCamZoomSize = outMechCamZoomSize; // sets default zoom size at start		
 	}
 
 	// Update is called once per frame
 	void LateUpdate() {
-		//Updates the zoom
-
-		mainCam.orthographicSize = mainCam.orthographicSize * camZoomK + targetCamZoomSize * (1.0f - camZoomK);
-
-	    transform.position = new Vector3 (myPlayer.transform.position.x, myPlayer.transform.position.y, -10);
+        // Updates the zoom
+        if (Mathf.Abs(mainCam.fieldOfView - targetCamZoomSize) > 0.1f) {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, targetCamZoomSize, zoomSpeed * Time.deltaTime);
+        }
+        
+        // Follows player around
+        Vector3 targetPosition = new Vector3(myPlayer.transform.position.x, myPlayer.transform.position.y, -10);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);       
 	}
 
 	//Adaptive zoom based on optional mech passed; uses default otherwise
 	public void MechZoom(Mech mech =  null) {
-		if (mech == null) targetCamZoomSize = 4;
-
-		else targetCamZoomSize = Mathf.Max(mechCamZoomSize, mech.transform.lossyScale.y);
+        if (mech == null) {
+            targetCamZoomSize = outMechCamZoomSize;
+        }
+        else {
+            targetCamZoomSize = inMechCamZoomSize;
+        }
 	}
 }
