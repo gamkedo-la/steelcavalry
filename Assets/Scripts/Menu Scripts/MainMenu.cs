9baa472d6521b31b39e936ed49926e5085880cdd
dviruet <dviruet@stetson.edu>
2018-02-07 13:38:34 -0500

    Added Menu Buttons & UFO Model

---
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void LevelTwo() {
+		SceneManager.LoadScene ("Level 2");
+	}
+
+	public void Credits() {
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void QuitGame() {
+		Application.Quit (); 
+	}
+
+}
