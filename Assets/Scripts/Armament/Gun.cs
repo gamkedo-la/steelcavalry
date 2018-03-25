f5c4749270a1173e29863399011126e0b5557a8f
Randy Tan Shaoxian <ranmaru90@gmail.com>
2018-03-26 02:35:04 +0800

    (gattling) Gun Turret rotates when firing

--- Assets/Scripts/Armament/Gun.cs
+++ Assets/Scripts/Armament/Gun.cs
@@ -3,79 +3,67 @@ using UnityEngine.Assertions;
 
 public class Gun : MonoBehaviour, IWeapon
 {
-	[SerializeField] private GameObject shell;
-	[SerializeField] private Transform shellSpawnPoint;
-	[SerializeField] private ParticleSystem muzzleFlesh;
-	[SerializeField] private GameEventAudioEvent audioEvent;
-	[SerializeField] private Transform spawnPoint;
-	[SerializeField] private GameEventUI weaponSlotEvents;
-	[SerializeField] private WeaponParameters parameters = null;
-	[SerializeField] private WeaponType type = WeaponType.Turret;
-	[SerializeField] private float minAngle = -60f;
-	[SerializeField] private float maxAngle = 60f;
-	[SerializeField] private GameObject[] models;
+    [SerializeField] private GameObject shell;
+    [SerializeField] private Transform shellSpawnPoint;
+    [SerializeField] private ParticleSystem muzzleFlesh;
+    [SerializeField] private GameEventAudioEvent audioEvent;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameEventUI weaponSlotEvents;
+    [SerializeField] private WeaponParameters parameters = null;
+    [SerializeField] private WeaponType type = WeaponType.Turret;
+    [SerializeField] private float minAngle = -60f;
+    [SerializeField] private float maxAngle = 60f;
+    [SerializeField] private GameObject model;
 
-	public WeaponType Type
-	{
-		get { return type; }
-	}
+    public WeaponType Type {
+        get { return type; }
+    }
 
-	private bool isActive = false;
-	private bool isRight = false;
-	private bool isPlayerDriver = false;
-	private float xAngle;
+    private bool isActive = false;
+    private bool isRight = false;
+    private bool isPlayerDriver = false;
+    private float xAngle;
 
-	private float timeToNextShot = 0;
-	private float realoadTimeLeft = 0;
-	private int currentMagSize = 0;
+    private float timeToNextShot = 0;
+    private float realoadTimeLeft = 0;
+    private int currentMagSize = 0;
 
-	void Start( )
-	{
-		Assert.IsNotNull( shell );
-		Assert.IsNotNull( shellSpawnPoint );
-		Assert.IsNotNull( muzzleFlesh );
-		Assert.IsNotNull( parameters );
-		Assert.IsNotNull( parameters.Projectile );
-		Assert.IsNotNull( spawnPoint );
-		Assert.IsNotNull( weaponSlotEvents );
-		Assert.IsNotNull( audioEvent );
+    void Start() {
+        Assert.IsNotNull(shell);
+        Assert.IsNotNull(shellSpawnPoint);
+        Assert.IsNotNull(muzzleFlesh);
+        Assert.IsNotNull(parameters);
+        Assert.IsNotNull(parameters.Projectile);
+        Assert.IsNotNull(spawnPoint);
+        Assert.IsNotNull(weaponSlotEvents);
+        Assert.IsNotNull(audioEvent);
 
-		currentMagSize = (int)parameters.MagSize;
-	}
+        currentMagSize = (int)parameters.MagSize;
+    }
 
-	void Update( )
-	{
-		if ( !isActive ) return;
+    void Update() {
+        if (!isActive) return;
 
-		realoadTimeLeft -= Time.deltaTime;
-		timeToNextShot -= Time.deltaTime;
+        realoadTimeLeft -= Time.deltaTime;
+        timeToNextShot -= Time.deltaTime;
 
-		if ( isPlayerDriver && realoadTimeLeft >= 0 )
-		{
-			weaponSlotEvents.Raise( UIEvent.TurretOn, 1f - ( realoadTimeLeft / parameters.RealoadTime ) );
-		}
-	}
+        if (isPlayerDriver && realoadTimeLeft >= 0) {
+            weaponSlotEvents.Raise(UIEvent.TurretOn, 1f - (realoadTimeLeft / parameters.RealoadTime));
+        }
+    }
 
-	void FixedUpdate( )
-	{
-		if ( !isActive ) return;
+    void FixedUpdate() {
+        if (!isActive) return;
 
-		LookAtCursor( );
-	}
+        LookAtCursor();
+    }
 
-	public void IsPlayerDriving( bool playerDriver )
-	{
-		isPlayerDriver = playerDriver;
-	}
+    public void IsPlayerDriving(bool playerDriver) {
+        isPlayerDriver = playerDriver;
+    }
 
-	public void SwapModel( string mechName )
-	{
-		if ( mechName == "Ostrich The Bot" )
-		{
-			models[0].SetActive( false );
-			models[1].SetActive( true );
-		}
-	}
+    public void SwapModel(string mechName) { }
+		
 
 	public void Active(bool isActive)
 	{
@@ -129,9 +117,12 @@ public class Gun : MonoBehaviour, IWeapon
 		s.GetComponent<Rigidbody2D>( ).AddForce( Quaternion.Euler( 0, 0, Random.Range( -30, 30 ) ) * spawnPoint.up * 15 );
 
 		timeToNextShot = parameters.DelayBetweenShots;
-		currentMagSize--;
 
-		if ( isPlayerDriver )
+        currentMagSize--;
+
+        model.transform.rotation = Quaternion.Euler(model.transform.eulerAngles.x, model.transform.eulerAngles.y, model.transform.eulerAngles.z + 10f);
+
+        if ( isPlayerDriver )
 		{
 			weaponSlotEvents.Raise( UIEvent.TurretOn, currentMagSize / parameters.MagSize );
 		}
