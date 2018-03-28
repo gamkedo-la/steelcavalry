67bc501e88d697da04f07fe89835f80b9597a12a
OmegaLarmor <remy-lapointe@hotmail.ca>
2018-03-27 21:16:11 -0400

    Enemy base level (unfinished) door addition and decorations

---
+++ Assets/MoveOnTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnTrigger : MonoBehaviour {
+
+	public Transform initialPos;
+	public Transform endPos;
+	private Transform target;
+
+	public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float delta = moveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, delta);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		Debug.Log("Triggered");
+		target = endPos;
+
+	}
+
+	void OnTriggerExit2D(){
+		Debug.Log("Exit");
+		target = initialPos;
+	}
+}
