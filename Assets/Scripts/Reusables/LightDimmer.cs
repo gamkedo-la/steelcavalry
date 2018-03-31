1fcfea82a4fc3d9528361b614c2a3e4c5d374f45
jeremyjackson89 <jeremy.jackson089@gmail.com>
2018-03-31 00:31:40 -0400

    blinking lights attached to mine prefab.

---
+++ Assets/Scripts/Reusables/LightDimmer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDimmer : MonoBehaviour {
+	private Light lightToDim;
+
+	private float minIntensity = 2;
+	private float maxIntensity = 8;
+	private float intensityStep = 0.05f;
+	private bool intensityIsIncreasing = true;
+	private int intensityModifier = 1;
+
+	void Start () {
+		lightToDim = this.GetComponent<Light>();
+		lightToDim.intensity = minIntensity;
+	}
+	
+	void Update () {
+		if (!lightToDim) return;
+		float currentIntensity = lightToDim.intensity;
+
+		if (intensityIsIncreasing && currentIntensity >= maxIntensity) {
+			intensityIsIncreasing = false;
+		}
+		if (!intensityIsIncreasing && currentIntensity <= minIntensity) {
+			intensityIsIncreasing = true;
+		}
+
+		intensityModifier = intensityIsIncreasing ? 1 : -1;
+
+		lightToDim.intensity += intensityStep * intensityModifier;
+	}
+}
