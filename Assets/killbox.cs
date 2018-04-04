bfcad7b45fa43cf3af05a9c49e93e541c0f39747
McFunkypants <breakdance@mcfunkypants.com>
2018-04-03 18:37:07 -0700

    level boundary colliders and killbox script (respawn if player falls too far)

---
+++ Assets/killbox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killbox : MonoBehaviour {
+
+	//private GameEventFloat didDamageEvent = null;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+
+		// Try to find a Mech script on the hit object
+		HP hp = collision.collider.GetComponent<HP>( );
+		if ( hp )
+		{
+			Debug.Log (collision.collider.gameObject.name + " hit a killbox!");
+			//didDamageEvent.Raise( 1f );
+			hp.TakeDamage( 9999999 );
+		}
+
+	}
+
+}
