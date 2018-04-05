d82be67b021ca578dcfcb7eabaa5a96b442d3597
jeremyjackson89 <jeremy.jackson089@gmail.com>
2018-04-04 22:34:59 -0400

    InGameMenu changes text and active buttons based on if Pause, MissionFailed, or MissionComplete is called.

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour {
 
 	public static bool gameIsPaused = false;
 	public GameObject inGameMenuUI;
+
 	private Scene currentScene;
+	private Text menuTitleText;
+
+	private GameObject resumeButton;
+	private GameObject retryButton;
+	private GameObject nextStageButton;
+
 	private string pauseText = "Paused";
 	private string playerDiedText = "Mission Failed";
 	private string playerWonText = "Mission Complete";
 
+	private List<string> sceneNames = new List<string>();
+	private int totalScenes = 3;
+
 	void Start () {
-		currentScene = SceneManager.GetActiveScene();
+		SetSceneAndMenuUI();
+		DeactivateMenu();
+		DeactivateButtons();
+
+		sceneNames.Add("Main Scene");
+		sceneNames.Add("Space Station");
+		sceneNames.Add("Enemy Base");
 	}
 
 	// Update is called once per frame
@@ -25,6 +42,30 @@ public class InGameMenu : MonoBehaviour {
 				Pause();
 			}
 		}
+		if (Input.GetKeyDown(KeyCode.X)) {
+			MissionFailed();
+		}
+		if (Input.GetKeyDown(KeyCode.C)) {
+			MissionComplete();
+		}
+	}
+
+	void SetSceneAndMenuUI () {
+		currentScene = SceneManager.GetActiveScene();
+		menuTitleText = GameObject.Find("MenuTitle").GetComponent<Text>();
+		resumeButton = GameObject.Find("ResumeButton");
+		retryButton = GameObject.Find("RetryButton");
+		nextStageButton = GameObject.Find("NextStageButton");
+	}
+
+	void DeactivateMenu () {
+		inGameMenuUI.SetActive(false);
+	}
+
+	void DeactivateButtons () {
+		resumeButton.SetActive(false);
+		retryButton.SetActive(false);
+		nextStageButton.SetActive(false);
 	}
 
 	public void Resume () {
@@ -36,9 +77,25 @@ public class InGameMenu : MonoBehaviour {
 
 	void Pause () {
 		inGameMenuUI.SetActive(true);
+		resumeButton.SetActive(true);
 		Time.timeScale = 0f;
 		gameIsPaused = true;
 		Cursor.visible = true;
+		menuTitleText.text = pauseText;
+	}
+
+	public void MissionFailed () {
+		Pause();
+		DeactivateButtons();
+		retryButton.SetActive(true);
+		menuTitleText.text = playerDiedText;
+	}
+
+	public void MissionComplete () {
+		Pause();
+		DeactivateButtons();
+		nextStageButton.SetActive(true);
+		menuTitleText.text = playerWonText;
 	}
 
 	public void ReloadScene () {
@@ -47,7 +104,12 @@ public class InGameMenu : MonoBehaviour {
 	}
 
 	public void GoToNextStage () {
-		// Handle going to the next stage
+		Time.timeScale = 1f;
+		int nextSceneIndex = sceneNames.IndexOf(currentScene.name) + 1;
+		if (nextSceneIndex <= totalScenes) {
+			string nextSceneName = sceneNames[nextSceneIndex];
+			SceneManager.LoadScene(nextSceneName);
+		}
 	}
 
 	public void LoadMenu () {
