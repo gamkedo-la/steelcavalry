e966946d03c64e26b0ce9a31d2572f764a8f8103
Chris DeLeon <gamkedo@gmail.com>
2018-04-06 20:10:01 -0700

    Additional tweaks to intro area

---
+++ Assets/ChangeToolTip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeToolTip : MonoBehaviour {
+	public Text toolTip;
+	
+	// Update is called once per frame
+	void OnDestroy() {
+		toolTip.text = "Press E to\nEnter/Exit mech";
+	}
+}
