d9607b3218b170d359cbe0dc3a1ff0fd557441f5
jeremyjackson89 <jeremy.jackson089@gmail.com>
2018-04-19 22:26:51 -0400

    LocalizationManager coded. English and Spanish buttons. Temp JSON files for translations.

--- Assets/Scripts/LocalizationManager.cs
+++ Assets/Scripts/LocalizationManager.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LocalizationManager : MonoBehaviour {
 
+	public static LocalizationManager instance;
+
+	private Dictionary<string, string> localizedText;
+	private bool isReady = false;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void LoadLocalizedText (string fileName) {
+		localizedText = new Dictionary<string, string>();
+		string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+		if (File.Exists(filePath)) {
+			string dataAsJson = File.ReadAllText(filePath);
+			LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+			for (int i = 0; i < loadedData.items.Length; i++){
+				string key = loadedData.items[i].key;
+				string value = loadedData.items[i].value;
+				localizedText.Add(key, value);
+			}
+
+			Debug.Log("Data loaded, dictionary contains: " + localizedText.Count + " entries") ;
+		} else {
+			Debug.LogError("Cannot find file '" + filePath + "'!");
+		}
+
+		isReady = true;
+	}
+
+	public bool getIsReady() {
+		return isReady;
 	}
 }
