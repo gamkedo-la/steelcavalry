9757289562ec27a38b24a1d91bfdf4781ab5cfb4
Chris DeLeon <gamkedo@gmail.com>
2018-05-08 12:30:02 -0700

    Set Esc key to exit game from credits or menu (only relevant for native build, important if playing fullscreen)

---
+++ Assets/Scripts/EscToMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscToMainMenu : MonoBehaviour {
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu"); 
+        }
+	}
+}
---
+++ Assets/Scripts/EscToQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscToQuit : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
